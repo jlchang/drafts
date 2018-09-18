356e2bb26da6c91bfe2770e101e22a153db2ea38
jlchang <jlchang@broadinstitute.org>
2018-09-17 22:24:02 -0400

    added check for required argument

--- methods_repo/run_sc3_test.R
+++ methods_repo/run_sc3_test.R
@@ -1,9 +1,9 @@
 #!/usr/bin/env Rscript
 
 # Load libraries
-suppressPackageStartupMessages(library('optparse''));
-suppressPackageStartupMessages(library('SC3'));
-suppressPackageStartupMessages(library('SingleCellExperiment'));
+suppressPackageStartupMessages(library("optparse"));
+suppressPackageStartupMessages(library("SC3"));
+suppressPackageStartupMessages(library("SingleCellExperiment"));
 
 # Command line arguments
 pargs <- optparse::OptionParser(usage = paste("%prog [options]",
@@ -12,13 +12,12 @@ pargs <- optparse::OptionParser(usage = paste("%prog [options]",
 # input (string) path to SingleCellExperiment object
 pargs <- optparse::add_option(pargs, c("--input"),
             type = "character",
-            default = "deng-reads.rds",
             action = "store",
             dest = "input",
             metavar = "Input",
             help = paste("Input file for analysis.",
-                         "Expect SingleCellExperiment object in rds file format",
-                         "[Default %default][REQUIRED]"))
+                         "SingleCellExperiment object in rds file format",
+                         "[REQUIRED]"))
                          
 pargs <- optparse::add_option(pargs, c("--ks"),
             type = "character",
@@ -26,10 +25,24 @@ pargs <- optparse::add_option(pargs, c("--ks"),
             action = "store",
             dest = "ks",
             metavar = "ks",
-            help = paste("a range of the number of clusters k used for SC3 clustering",
-                         "Can also be a single integer.", "[Default %default]"))
+            help = paste("range of number of clusters, ",
+                         "k, used for SC3 clustering",
+                         "Can also be a single integer.",
+                         "[Default %default]"))
+                     
+# Check arguments to ensure user input meets certain requirements.    
+check_arguments <- function(arguments){
+    if ( (!( "input" %in% names(arguments))) ||
+         (arguments$input == "") ||
+         (is.na(arguments$input)) ) {
+      stop("error, no --input")
+    }
+  return(arguments)
+}
                          
 args_parsed <- optparse::parse_args(pargs)
+
+args_parsed <- check_arguments(args_parsed)
 
 
 # Make sure the input data exists
@@ -40,10 +53,19 @@ if (!file.exists(args_parsed$input[1])){
     stop(error_message)
 }
 
+#Define output name based on input name. 
+#If path to file is provided, path is maintained
 input_file <- unlist(strsplit(args_parsed$input[1], "[.]"))
-output_file <- paste0(input_file[1:length(input_file)-1],"-SC3.",input_file[length(input_file)])
+output_file <- paste0(input_file[1:length(input_file) - 1],
+                      "-SC3.", input_file[length(input_file)])
 
-#log ks used?
+#read in data
 sce <- readRDS(args_parsed$input[1]);
-sce <- sc3(sce, ks=args_parsed$ks[1], biology=T);
+
+#run method on data
+sce <- sc3(sce, ks = args_parsed$ks[1], biology = T);
+
+#generate data
+
+#save data to current working directory
 saveRDS(sce, output_file)
