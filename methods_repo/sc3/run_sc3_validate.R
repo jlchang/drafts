22ef397b60dca070b4345363cb3fb9d1839b0456
jlchang <jlchang@broadinstitute.org>
2019-01-21 08:29:18 -0500

    parameterize inputs, incorporate here usage

--- methods_repo/sc3/run_sc3_validate.R
+++ methods_repo/sc3/run_sc3_validate.R
@@ -2,9 +2,20 @@
 
 # Load libraries
 suppressPackageStartupMessages(library("optparse"));
+suppressPackageStartupMessages(library("logging"));
+suppressPackageStartupMessages(library("here"));
 suppressPackageStartupMessages(library("SC3"));
 suppressPackageStartupMessages(library("SingleCellExperiment"));
-suppressPackageStartupMessages(library("logging"));
+
+
+# Data sources
+## input data for demo and validation
+demo_input_source <- "https://github.com/hemberg-lab/scRNA.seq.course/raw/master/deng/deng-reads.rds"
+demo_input <- here("deng-reads.rds")
+## reference output for validation
+## TEMPORARY LOCATION for script development
+reference_source <- "https://raw.githubusercontent.com/jlchang/drafts/master/methods_repo/sc3/reference_output.csv"
+reference_output <- here("reference_output.csv")
 
 # Set up logging
 # Logging level choices
@@ -80,9 +91,8 @@ pargs <- optparse::add_option(pargs, c("--stdoutfile"),
 args_parsed <- optparse::parse_args(pargs)
 
 if (args_parsed$demo[1] || args_parsed$validate[1]){
-  download.file("https://github.com/hemberg-lab/scRNA.seq.course/raw/master/deng/deng-reads.rds",
-                "deng-reads.rds")
-  input_file <- "deng-reads.rds"
+  download.file(demo_input_source, demo_input)
+  input_file <- demo_input
 } else {
   input_file <- args_parsed$input[1]
 }
@@ -110,52 +120,71 @@ output_file <- paste0(dir_in, "/", "SC3-", base_in)
 
 if (args_parsed$demo[1]){
   args_parsed$cell_labels[1] <- TRUE
-  labels_file <- "data/demo_labels.csv"
+  labels_file <- "validation.csv"
 } else {
-  labels_file <- "data/sc3_labels.csv"
+  labels_file <- "sc3_labels.csv"
 }
 
 #read in data
 sce <- readRDS(input_file);
 
-#run method on data
+#for demo mode, run SC3 on demo data with k=10 to match on-line example
 if (args_parsed$demo[1]){
     args_parsed$ks[1] <- 10
     sce <- sc3(sce, ks = args_parsed$ks[1], biology = T,
                rand_seed = args_parsed$seed[1], n_cores = 1);
+
+#for validation, only run SC3 protocol to last deterministic step, sc3_calc_transfs
 } else if (args_parsed$validate[1]){
     sce <- sc3_prepare(sce, rand_seed = args_parsed$seed[1], n_cores = 1)
     sce <- sc3_calc_dists(sce)
     sce <- sc3_calc_transfs(sce)
-    rounded_data <- round(metadata(sce)$sc3$transformations$pearson_laplacian,
-                          digits = 8)
-    transfs_result <- cbind(colnames(sce), rounded_data)
-    write.table(transfs_result, file = "data/validation.csv",
+
+    #TEMPORARY home for reference output
+    download.file(reference_source, reference_output)
+    ref <- read.table(reference_output, header = FALSE, row.names = 1, sep = ",")
+    #check calls (row names) match between reference and validation output
+    if (isTRUE(all.equal(colnames(sce),rownames(ref)))) {
+      logging::loginfo(paste("Cell names in validation output match reference"))
+    #if cells don't match, make validation and reference files available and exit
+    } else {
+      transfs_result <- cbind(colnames(sce), metadata(sce)$sc3$transformations$pearson_laplacian)
+      write.table(transfs_result, file = here("data", "validation.csv"),
                 row.names = FALSE, col.names = FALSE,
                 quote = FALSE, sep = ",")
-    download.file("https://raw.githubusercontent.com/jlchang/drafts/master/methods_repo/sc3/reference_output.csv",
-                  "data/reference_output.csv")
-    if (isTRUE(all.equal(readLines("data/reference_output.csv"),
-                         readLines("data/validation.csv")))) {
+      file.copy(reference_output, here("data", reference_output))
+      stop("validation failed - cell names in validation and reference data do not match")
+    }
+    #coerce the reference data in preparation for calculating relative error
+    test_matrix <- as.matrix(ref)
+
+    #check whether largest relative error is larger than acceptable threshold
+    if (max( abs((as.matrix(metadata(sce)$sc3$transformations$pearson_laplacian)) 
+         - test_matrix) / abs(test_matrix)) < 1.0e-8) {
       logging::loginfo(paste("Successful validation - validation file matches reference"))
+    #if max relative error exceeds threshold, make validation and reference files available and exit
     } else {
-      stop("validation failed")
+      transfs_result <- cbind(colnames(sce), metadata(sce)$sc3$transformations$pearson_laplacian)
+      write.table(transfs_result, file = here("data", "validation.csv"),
+                row.names = FALSE, col.names = FALSE,
+                quote = FALSE, sep = ",")
+      file.copy(reference_output, here("data", reference_output))
+      stop("validation failed - max relative error in validation data exceeds threshold")
     }
 } else {
   sce <- sc3(sce, ks = args_parsed$ks[1], biology = T);
+  #save data to current working directory
+  saveRDS(sce, output_file)
+  logging::loginfo(paste("SingleCellExperiment object", here("data", here("data", output_file),
+                       "saved", sep = " "))
 }
 
-
-#save data to current working directory
-saveRDS(sce, output_file)
-logging::loginfo(paste("SingleCellExperiment object", output_file,
-                       "saved", sep = " "))
 
 if (args_parsed$cell_labels[1]) {
   cluster_Col <- paste0("sc3_", args_parsed$ks[1], "_clusters")
   labels <- cbind(colnames(sce), eval(parse(text = paste0("colData(sce)$",
                                                           cluster_Col))))
-  write.table(labels, file = labels_file, row.names = FALSE,
+  write.table(labels, file = here("data", labels_file), row.names = FALSE,
               col.names = FALSE, quote = FALSE, sep = ",")
   logging::loginfo(paste("Cell labels saved in", output_file, sep = " "))
 }
